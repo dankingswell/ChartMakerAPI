32b3ee91d188dece55a10a99557ba57cd91f564e
Dan Kingswell <dankingswell22@gmail.com>
2018-10-09 12:43:49 +0100

    puppeteer tests

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const publicPath = Path.join(__dirname,"..","public")
 const port = process.env.PORT || 8000;
 const SvgMaker= require("./SVGmaker")
 const PNGMaker = require("./ToPNG")
+const puppeteer = require('puppeteer');
 
 
 
@@ -20,20 +21,23 @@ app.get("*", (req,res) => {
 var valuesGiven= []
 for (x in data){
     valuesGiven.push(Number(data[x]))
-}
+}   
+    (async() => {
+    const browser = await puppeteer.launch({
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    })})
+
     SvgMaker(valuesGiven)
     PNGMaker().then(()=>{
 
     res.set("Content-Type",'image/jpeg')
     res.sendFile(Path.join(__dirname,"/chart.jpeg"))
     console.log("done")
+    })
 
-})
-    
 
 });
 
 app.listen(port , ()=>{
     console.log("Server live")
 });
-
